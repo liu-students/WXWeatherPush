628baaa619fa77216724553f6f72dc650f5c38dc
liu-students <141470007+liu-students@users.noreply.github.com>
2023-08-05 17:02:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -119,8 +119,8 @@ def get_ciba():
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
     r = get(url, headers=headers)
-    note_en = r.json()["content"]
     note_ch = r.json()["note"]
+    note_en = r.json()["content"]
     return note_ch, note_en
  
                 
